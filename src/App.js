63633e196871772de28755afa323eb7d9acb994d
t3ugeg00 <t3ugeg00@students.oamk.fi>
2024-10-28 12:23:12 +0200

    Part1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useState } from 'react';
+
 function App() {
+
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  function inputChange(event) {
+    setTask(event.target.value);
+  }
+
+  function keyPressed(event) {
+    if (event.key == 'Enter') {
+      event.preventDefault();
+
+      let setter = [...tasks];
+      setter.push(task);
+
+      setTask('');
+
+      setTasks([...setter]);
+    }
+  }
+
+  function remover(taskName) {
+    const setter = tasks.filter(element => element != taskName);
+
+    setTasks([...setter]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <h3>Todos</h3>
+      <form>
+        <input placeholder='Your task' onChange={inputChange} onKeyDown={keyPressed} value={task}/>
+      </form>
+      <ul>
+        {tasks.map(element => <li>
+          {element} <button className='delete-button' onClick={() => {remover(element)}}>Delete</button>
+        </li>)}
+      </ul>
     </div>
   );
 }
