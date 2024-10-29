1b7af60ce1fbc5ec79747b13e00c3ac7609fb493
t3ugeg00 <t3ugeg00@students.oamk.fi>
2024-10-29 15:24:26 +0200

    Part4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
 
-import { useState } from 'react';
+import Row from './Row';
+
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const URL = 'http://localhost:3001/';
 
 function App() {
 
   const [task, setTask] = useState('');
   const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    axios.get(URL)
+    .then(response => {
+      setTasks(response.data);
+    }).catch(error => {
+      alert(error.response.data.error ? error.response.data.error : error);
+    })
+  },[]);
 
   function inputChange(event) {
     setTask(event.target.value);
@@ -15,19 +29,27 @@ function App() {
     if (event.key == 'Enter') {
       event.preventDefault();
 
-      let setter = [...tasks];
-      setter.push(task);
-
-      setTask('');
-
-      setTasks([...setter]);
+      axios.post(URL + 'create',{
+        description: task
+      })
+      .then(response => {
+        setTasks([...tasks, {id: response.data.id, description: response.data.description}]);
+        setTask('');
+      }).catch(error => {
+        alert(error.response.data.error ? error.response.data.error : error);
+      })
     }
   }
 
-  function remover(taskName) {
-    const setter = tasks.filter(element => element != taskName);
-
-    setTasks([...setter]);
+  function remover(id) {
+    axios.delete(`${URL}delete/${id}`)
+    .then(response => {
+      setTasks(
+        tasks.filter(element => element.id != id)
+      )
+    }).catch(error => {
+      alert(error.response.data.error ? error.response.data.error : error);
+    })
   }
 
   return (
@@ -37,9 +59,9 @@ function App() {
         <input placeholder='Your task' onChange={inputChange} onKeyDown={keyPressed} value={task}/>
       </form>
       <ul>
-        {tasks.map(element => <li>
-          {element} <button className='delete-button' onClick={() => {remover(element)}}>Delete</button>
-        </li>)}
+        {tasks.map(element =>
+          <Row key={element.id} element={element} remover={remover}/>
+        )}
       </ul>
     </div>
   );
---
+++ src/Row.js
@@ -0,0 +1,10 @@
+import './App.css';
+
+export default function Row({ element, remover }) {
+    return (
+        <li key={element.id}>
+            {element.description}
+            <button className='delete-button' onClick={() => {remover(element.id)}}>Delete</button>
+        </li>
+    )
+}
